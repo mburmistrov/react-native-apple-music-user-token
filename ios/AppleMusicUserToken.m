9050b06f824f64911bc1971675d2e064580c35d4
Mikhail Burmistrov <misha.burmistrov@gmail.com>
2020-02-27 15:59:52 +0300

    implement requestUserTokenForDeveloperToken method

--- ios/AppleMusicUserToken.m
+++ ios/AppleMusicUserToken.m
@@ -1,14 +1,21 @@
 #import "AppleMusicUserToken.h"
+#import <StoreKit/StoreKit.h>
 
 
 @implementation AppleMusicUserToken
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(requestUserTokenForDeveloperToken:(NSString *)developerToken resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    SKCloudServiceController *cloudController = [[SKCloudServiceController alloc] init];
+    [cloudController requestUserTokenForDeveloperToken:developerToken completionHandler:^(NSString *userToken, NSError *error) {
+        if (error == nil && userToken != nil) {
+            resolve(userToken);
+        } else {
+            reject(@"user_token_request_failed", @"User token request failed", error);
+        }
+    }];
 }
 
 @end
