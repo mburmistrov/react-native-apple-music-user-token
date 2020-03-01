63d944290db363a7c60ed9babd8d86851f47e6f3
Mikhail Burmistrov <misha.burmistrov@gmail.com>
2020-03-02 02:08:21 +0300

    implement requestAuthorization method

--- ios/AppleMusicUserToken.m
+++ ios/AppleMusicUserToken.m
@@ -1,10 +1,28 @@
 #import "AppleMusicUserToken.h"
 #import <StoreKit/StoreKit.h>
 
-
 @implementation AppleMusicUserToken
 
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(requestAuthorization:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+{
+    [SKCloudServiceController requestAuthorization:^(SKCloudServiceAuthorizationStatus status) {
+        switch (status) {
+            case SKCloudServiceAuthorizationStatusDenied:
+                reject(@"authorization_denied", @"Authorization denied", nil);
+                break;
+            case SKCloudServiceAuthorizationStatusRestricted:
+                reject(@"authorization_restricted", @"Authorization restricted", nil);
+                break;
+            case SKCloudServiceAuthorizationStatusAuthorized:
+                resolve(nil);
+                break;
+            default:
+                break;
+        }
+    }];
+}
 
 RCT_EXPORT_METHOD(requestUserTokenForDeveloperToken:(NSString *)developerToken resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
